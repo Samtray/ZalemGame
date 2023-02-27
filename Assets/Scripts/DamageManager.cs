968d0fb4b44e7d47bba966a30538f8ec253db59b
Samtray <sammychurro@gmail.com>
2023-02-26 18:57:03 -0800

    a

--- Assets/Scripts/DamageManager.cs
+++ Assets/Scripts/DamageManager.cs
@@ -6,19 +6,34 @@ using UnityEngine.SceneManagement;
 public class DamageManager : MonoBehaviour
 {
     public int health;
-    public int maxHealth = 3; 
-    private int sceneBuildIndex;
+    public int maxHealth; 
+    public int sceneBuildIndex;
     private Rigidbody2D victimRigidbody;
     public Vector2 damageTakenForce;
     public int invincibilityDuration;
     private bool invincible; 
     private SpriteRenderer spriteComponent;
+    public float speed;
+    private Color baseColor;
     void Start()
     {
+        maxHealth = 3;
         invincible = false;
         health = maxHealth;
         victimRigidbody = gameObject.GetComponent<Rigidbody2D>();
         spriteComponent = gameObject.GetComponent<SpriteRenderer>();
+        baseColor = spriteComponent.color;
+    }
+
+    private void Update()
+    {
+        if (invincible)
+        {
+            spriteComponent.color = Color.Lerp(Color.clear, baseColor, Mathf.PingPong(Time.time * speed, 1));
+        }
+        else {
+            spriteComponent.color = baseColor;
+        }
     }
 
     public void takeDamage(int damage){
