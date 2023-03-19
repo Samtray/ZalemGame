e8f3fbb7a0d5ecee749c47a3a08357a084e03db3
Samtray <sammychurro@gmail.com>
2023-03-18 17:27:27 -0700

    Qol

--- Assets/Scripts/CheckGround.cs
+++ Assets/Scripts/CheckGround.cs
@@ -8,6 +8,13 @@ public class CheckGround : MonoBehaviour
     public static bool terreno;
     public static bool platform;
 
+
+    private void Start()
+    {
+        terreno = false;
+        platform = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.CompareTag("Terreno")) terreno = true;
         if (collision.gameObject.CompareTag("Platform")) platform = true;
